677e391bbbfa220f49802e7f8bb66f3444665fec
ftharsh <hariharshvardhan22@gmail.com>
2024-08-18 09:32:36 +0530

    Logo changed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -87,7 +87,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Harshavardhan</Logo>
+        <Logo>Vikram_pratap_singh</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
